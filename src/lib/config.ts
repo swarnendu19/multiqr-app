0562c9fb77cef727acdd526fe5be75ea46babb9e
Swarnendu <swarnendumaity633@gmail.com>
2025-12-26 17:32:10 +0530

    feat: Initialize project with Prisma schema for user, QR project, and scan models, and add application configuration.

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:3000';
-export const APP_URL = process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000';
+export const API_URL = 'https://multiqr-app.vercel.app/';
+export const APP_URL = process.env.NEXT_PUBLIC_APP_URL || 'https://multiqr-app.vercel.app/';
